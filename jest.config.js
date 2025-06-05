9dd8ba8cc02bf79032893b4e397bba3adae8949a
Alex Huang <alexxpasta@gmail.com>
2025-06-05 20:39:24 +0800

    update jest config to support absolute path import

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,7 @@ module.exports = {
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '^app/(.*)$': '<rootDir>/app/$1'
+  }
 }; 
