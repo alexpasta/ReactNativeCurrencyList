fc355afc5a5a0f774cd9abadf361caabefeea14e
Alex Huang <alexxpasta@gmail.com>
2025-06-05 11:17:34 +0800

    Update search logic

--- app/utils/currencyFilter.ts
+++ app/utils/currencyFilter.ts
@@ -1,13 +1,13 @@
 import { CurrencyInfo } from "../models/CurrencyInfo";
 
 export function filterCurrencyList(data: CurrencyInfo[], searchTerm: string): CurrencyInfo[] {
-    if (!searchTerm) return data;
-  
-    const term = searchTerm.toLowerCase();
-    return data.filter((currency) => {
-      if (currency.name.toLowerCase().startsWith(term)) return true;
-      if (currency.name.toLowerCase().includes(` ${term}`)) return true;
-      if (currency.symbol.toLowerCase().startsWith(term)) return true;
-      return false;
-    });
-  }
+  if (!searchTerm) return data;
+
+  const term = searchTerm.toLowerCase();
+  return data.filter((currency) => {
+    if (currency.name.toLowerCase().includes(term)) return true;
+    if (currency.symbol.toLowerCase().includes(term)) return true;
+    if (currency.code?.toLowerCase().includes(term)) return true;
+    return false;
+  });
+}
